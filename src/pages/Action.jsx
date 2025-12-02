11b7ca91d7e60135f1e3c3ec2d00df034f2d309b
Ali-bis <140365096+Ali-bis@users.noreply.github.com>
2025-12-02 15:21:22 +0530

    Update Action.jsx

--- src/pages/Action.jsx
+++ src/pages/Action.jsx
@@ -4,7 +4,7 @@ import Carousel from '../components/Carousel';
 const Action = () => {
   const actionImages = [
     { src: "/images/4.png.jpg", caption: "Workshop 1: Newsletter Writing" },
-    { src: "/images/2.png", caption: "Workshop 2: Word Search & Vocab" },
+    { src: "/images/5.png", caption: "Workshop 2: Word Search & Vocab" },
     { src: "/images/3.png", caption: "Workshop 3: Comic Strip Creation" }
   ];
 
